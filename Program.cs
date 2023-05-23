759ba4b989c4de2e8f785f2a9ea84cf659ee1e81
BDevOne <115705346+BDevOne@users.noreply.github.com>
2023-05-23 17:08:13 -0300

    Implementação CPF Concluída
    Foi concluída a implementação do CPF

--- Program.cs
+++ Program.cs
@@ -14,12 +14,9 @@ namespace CadastroPessoa
             Pad p1;
             Pad p3;
             Pad p2;
-         // Pad p4; <- Usuário de teste
 
-            // Implementar mais usuários para cadastro 
-            // Cadastrar usuário
-            // Solicitar informações do usuário (Documentos)
-            // Adicionar telefone do usuário -- Criar método de solicitação de telefone
+            // Implentar API de busca por usuário
+            // Adicionar telefone do usuário -- Criar método de solicitação de telefone 
             // Implementar Data de nascimento <- (Em andamento...) -- observações do usuário
 
             Console.WriteLine("####Cadastro de usuário####\n");
@@ -31,7 +28,7 @@ namespace CadastroPessoa
             p1 = new Pad();
             p1.Nome = Console.ReadLine();
             Console.Write("\nCPF do usuário: \n");
-            p1.Cpf = Convert.ToInt64(Console.ReadLine());
+            p1.Cpf = Console.ReadLine();
             Console.Write("\nIdade do 1 usuário: \n");
             p1.Idade = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nSexo do usuário: \n");
@@ -45,7 +42,7 @@ namespace CadastroPessoa
             p2 = new Pad();
             p2.Nome = Console.ReadLine();
             Console.Write("\nCPF do usuário: \n");
-            p2.Cpf = Convert.ToInt64(Console.ReadLine());
+            p2.Cpf = Console.ReadLine();
             Console.Write("\nIdade do 2 usuário: \n");
             p2.Idade = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nSexo do usuário: \n");
@@ -58,83 +55,36 @@ namespace CadastroPessoa
             p3 = new Pad();
             p3.Nome = Console.ReadLine();
             Console.Write("\nCPF do usuário: \n");
-            p3.Cpf = Convert.ToInt64(Console.ReadLine());
+            p3.Cpf = Console.ReadLine();
             Console.Write("\nIdade do 3 usuário: \n");
             p3.Idade = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nSexo do usuário: \n");
             p3.Sexo = Console.ReadLine();
 
-            //Console.WriteLine("\nInformações do usuário 4\n");
-
-            // Usuário 4 - Usuário de teste  
-            //Console.Write("Nome do 4 usuário: \n");
-            //p4 = new Pad();
-            //p4.Nome = Console.ReadLine();
-            //Console.Write("\nCPF do usuário: \n");
-            //p4.Cpf = Convert.ToInt64(Console.ReadLine());
-            //Console.Write("\nIdade do 4 usuário: \n");
-            //p4.Idade = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("\nSexo do usuário: \n");
-            //p4.Sexo = Console.ReadLine();
-            // int PhoneNumber = Convert.ToInt32(Console.ReadLine()); 
-
-            // Usuário cadastrado - Não foi possível cadastrar
+           // Lógica do Cadastro 
 
             Console.WriteLine("\n#Dados dos usuários#\n");
 
-            if ((p1.Idade >= 18))
-            {
-                Console.WriteLine("\nUsuário 1 cadastrado \n"); // Se for maior ou igual a 18 cadastrar usuário
-                p1.ExibirDados();
-            }
-            else
-            {
-                Console.WriteLine("\nNão foi possível cadastrar usuário 1 \n"); // Se o valor for menor que 18 exiber mensagem
-                p1.ExibirDados();
-            }
+            Pad[] pessoas = { p1, p2, p3 };
 
+            for (int i = 0; i < pessoas.Length; i++)
             {
-                if ((p2.Idade >= 18))
+                if (pessoas[i].Idade >= 18)
                 {
-                    Console.WriteLine("\nUsuário 2 cadastrado \n"); // Se for maior ou igual a 18 cadastrar usuário
-                    p2.ExibirDados();
+                    Console.WriteLine($"\nUsuário {i + 1} cadastrado \n");
+                    pessoas[i].ExibirDados();
                 }
                 else
                 {
-                    Console.WriteLine("\nNão foi possível cadastrar usuário 2 \n"); // Se o valor for menor que 18 exiber mensagem
-                    p2.ExibirDados();
+                    Console.WriteLine($"\nNão foi possível cadastrar usuário {i + 1} \n");
+                    pessoas[i].ExibirDados();
                 }
-
-                if ((p3.Idade >= 18))
-                {
-                    Console.WriteLine("\nUsuário 3 cadastrado \n"); // Se for maior ou igual a 18 cadastrar usuário
-                    p3.ExibirDados();
-                }
-                else
-                {
-                    {
-                        Console.WriteLine("\nNão foi possível cadastrar usuário 3 \n"); // Se o valor for menor que 18 exiber mensagem
-                        p3.ExibirDados();
-                    }
-
-                    //if ((p4.Idade >= 18))
-                    //{
-                    //    Console.WriteLine("\nUsuário 4 cadastrado \n"); // Se for maior ou igual a 18 cadastrar usuário
-                    //    p4.ExibirDados();
-                    //}
-                    //else
-                    //{
-                    //    {
-                    //        Console.WriteLine("\nNão foi possível cadastrar usuário 4 \n"); // Se o valor for menor que 18 exiber mensagem
-                    //        p4.ExibirDados();
-                    //    }
-
-                    //}
-                }
-                Console.ReadLine();
             }
+
+            Console.ReadLine();
         }
     }
 }
+
 
 
