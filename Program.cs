d0ceecc5d2a40e8492121e431e9f7cb15016509e
BDevOne <115705346+BDevOne@users.noreply.github.com>
2024-03-05 18:01:08 -0300

    Correções 05/03/2024 Program.cs
    Verificar no README as correções feitas.

--- Program.cs
+++ Program.cs
@@ -9,13 +9,11 @@ namespace CadastroPessoa
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Users cadastro = new Users();
 
-           var cadastro = new Users();
-           var cadUser = cadastro;
-
-           // bool seguirCad = true; -- Implemantar cadastros multiplos
+            // bool seguirCad = true; -- Implemantar cadastros multiplos
 
             Console.WriteLine("####Cadastro de usuário####\n");
 
@@ -27,46 +25,62 @@ namespace CadastroPessoa
             Console.Write("\nCPF do usuário: ");
             cadastro.Cpf = Console.ReadLine();
 
-            // Console.Write("\nData de nascimento do usuário: ");
-            // cadastro.Data = Console.ReadLine();
+            #region notUsed
+            Console.Write("\nData de nascimento do usuário: ");
+            cadastro.Data = Console.ReadLine();
 
             Console.Write("\nIdade do usuário: ");
             cadastro.Idade = Convert.ToInt32(Console.ReadLine());
 
-            // Console.Write("\nTelefone do usuário: ");
-            // cadastro.Telefone = Console.ReadLine();
+            Console.Write("\nTelefone do usuário: ");
+            cadastro.Telefone = Console.ReadLine();
 
-            // Console.Write("\nSelecione o sexo do usuário  (M / F): ");
-            // cadastro.Sexo = Console.ReadLine();
+            Console.Write("\nSelecione o sexo do usuário  (M / F): ");
+            cadastro.Sexo = Console.ReadLine();
+
 
             Console.Clear();
+            #endregion
 
-            Console.WriteLine("\n#Dados de Cadastro\n");
+            Console.WriteLine("\n#Dados de Cadastro#\n");
 
-            var idadeVerificar = cadUser.Idade >= 18; 
-            var cpfVerificar = cadUser.Cpf != cadastro.Cpf;
+            cadastro.RemoveEspecialCharacters();
 
-            if (idadeVerificar && cpfVerificar)
+            var cpfVerificar = !string.IsNullOrEmpty(cadastro.Cpf) && cadastro.Cpf.Length == 11; // Correção necessária - Alterar lógica de validação da idade e CPF.
+            var idadeVerificar = cadastro.Idade >= 18;
+
+            if (cpfVerificar == true)
             {
-                Console.WriteLine($"\nUsuário {cadastro.Nome} Cadastrado\n");
-                cadUser.ExibirDados();
+                cadastro.AddEspecialCharacters();
+                if (idadeVerificar)
+                {
+                    Console.WriteLine($"\nUsuário {cadastro.Nome} Cadastrado\n");
+                    cadastro.ExibirDados();
+                }
+                else
+                {
+                    if (!idadeVerificar)
+                    {
+                        Console.WriteLine($"\nNão foi possível cadastrar usuário {cadastro.Nome}\n");
+                        cadastro.ExibirDados();
+
+                        Console.WriteLine($"\nIdade não permitida: {cadastro.Idade}");
+                    }
+                }
             }
-            else 
+            else if (cpfVerificar == false)
             {
                 Console.WriteLine($"\nNão foi possível cadastrar usuário {cadastro.Nome}\n");
-                cadUser.ExibirDados();
-
-                if (!idadeVerificar)
+                if (idadeVerificar == false)
                 {
+                    cadastro.ExibirDados();
+
                     Console.WriteLine($"\nIdade não permitida: {cadastro.Idade}");
                 }
-                if (!cpfVerificar)
-                {
-                    Console.WriteLine($"\nCPF Inválido");
-                }
+                Console.WriteLine($"\nCPF Inválido: {cadastro.Cpf}");
             }
-            
             Console.ReadLine();
+
         }
     }
 }
